2b819e898afd2a69929f8f37a1a30e89426cd131
naorAlkobi27 <naor@webz.io>
2024-01-04 16:42:24 +0200

    update Redis to use connection pool

--- server.py
+++ server.py
@@ -15,13 +15,21 @@ logger = logging.getLogger(__name__)
 async def is_duplicate(request: Request):
     try:
         is_duplicate_document = False
+
+        # get parameters from request
         json_data = await request.json()
         content = json_data.get('content')
         article_domain = json_data.get('domain')
         language = json_data.get('language')
         article_id = json_data.get('article_id', None)
-        lsh = get_lsh_from_redis(lsh_key=f"{language}:lsh_index")
-        minhash = minhash_signature(content)
+        lsh_key = f"{language}:lsh_index"
+
+        # get lsh from redis
+        lsh = get_lsh_from_redis(lsh_key=lsh_key)
+
+        # calculate minhash for the new text
+        minhash = await minhash_signature(content)
+
         candidate_pairs = lsh.query(minhash)
         for candidate_pair in candidate_pairs:
             _id = candidate_pair.split('|')[0]
@@ -31,8 +39,9 @@ async def is_duplicate(request: Request):
                 is_duplicate_document = True
             else:
                 print(f"Found Similarity {article_domain} and {domain}")
+
         # add new minhash to lsh and store in Redis.
-        update_lsh_in_redis(lsh=lsh, lsh_key=f"{language}:lsh_index", minhash=minhash, article_id=article_id,
+        update_lsh_in_redis(lsh=lsh, lsh_key=lsh_key, minhash=minhash, article_id=article_id,
                             article_domain=article_domain)
 
         return JSONResponse(content={"is_duplicate_document": is_duplicate_document})
