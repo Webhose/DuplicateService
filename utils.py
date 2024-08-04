c61982cc66945dd5b91155c69136b19c9004c993
Naor Alkobi <naor@webz.io>
2024-08-04 17:54:59 +0300

    create fast recovery process

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pickle
 import string
+from datetime import datetime, timedelta
 import nltk
 from consts import Consts
 from minhash_lsh_ttl import MinHashLSHTTL
@@ -11,6 +12,9 @@ import logging
 import time
 import os
 import metrics3_docker.metrics as metrics
+from elasticsearch import Elasticsearch
+import asyncio
+from aiomultiprocess import Pool
 
 redis_pool = ConnectionPool(host=Consts.REDIS_HOST, port=Consts.REDIS_PORT, db=Consts.REDIS_DB)
 
@@ -53,15 +57,30 @@ except LookupError:
 
 # Decorator to measure function execution time
 def timeit_decorator(func):
-    def wrapper(*args, **kwargs):
+    """
+    Decorator to time the execution of a function, supporting both async and sync functions.
+    """
+
+    async def async_wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        logger.info(f"Async function '{func.__name__}' took {execution_time:.4f} seconds to execute.")
+        return result
+
+    def sync_wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        execution_time = (end_time - start_time)
+        execution_time = end_time - start_time
         logger.info(f"Function '{func.__name__}' took {execution_time:.4f} seconds to execute.")
         return result
 
-    return wrapper
+    if asyncio.iscoroutinefunction(func):
+        return async_wrapper
+    else:
+        return sync_wrapper
 
 
 # Preprocess and tokenize the input text
@@ -83,6 +102,151 @@ async def minhash_signature(document, language, num_perm=128):
     return minhash
 
 
+def get_es_connection():
+    try:
+        client = Elasticsearch("http://spirit-004:9200")
+        return client
+    except Exception as e:
+        print(f"Failed to connect to Elasticsearch with the following error: {e}")
+        return None
+
+
+def get_query(scroll_id=None, page_size=500, max_hours=12):
+    today = datetime.now()
+    yesterday = today - timedelta(hours=max_hours)
+    today = today.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    yesterday = yesterday.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+    query = {
+        "_source": ["text", "_id", "thread.site"],
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"thread.site_type": "news"}},
+                    {"match": {"language": "english"}},
+                    {"match": {"is_first": True}},
+                    {
+                        "range": {
+                            "sys_info.crawled": {
+                                "format": "strict_date_optional_time",
+                                "gte": yesterday,
+                                "lte": today
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        "size": page_size
+    }
+
+    if scroll_id:
+        query["_scroll_id"] = scroll_id
+    return query
+
+
+async def get_texts_from_es(language="english"):
+    """
+    Retrieve texts from Elasticsearch and insert MinHash signatures into LSH.
+    """
+    documents = []
+    es_client = get_es_connection()
+    if not es_client:
+        return documents
+
+    # Perform the initial search to get the initial scroll ID
+    initial_query = get_query()
+    try:
+        result = es_client.search(index="webhose*", body=initial_query, scroll="5m")
+    except Exception as e:
+        logger.error(f"Error during initial search: {e}")
+        return documents
+
+    scroll_id = result.get("_scroll_id")
+    total_hits = result["hits"]["total"]["value"]
+
+    total_pages = total_hits // 500 + 1
+
+    for i in range(total_pages):
+        logger.info(f"Processing page {i + 1} of {total_pages}")
+        try:
+            # Use the scroll ID to retrieve the next batch of results
+            result = es_client.scroll(scroll_id=scroll_id, scroll="5m")
+        except Exception as e:
+            logger.error(f"Error during scrolling: {e}")
+            break
+
+        scroll_id = result.get("_scroll_id")
+        hits = result["hits"]["hits"]
+
+        if not hits:
+            break  # No more results, break out of the loop
+
+        for hit in hits:
+            text = hit.get("_source", {}).get("text")
+            if not text:
+                continue
+            article_id = hit.get("_id")
+            article_domain = hit.get("_source", {}).get("thread", {}).get("site")
+            documents.append({
+                "article_id": article_id,
+                "article_domain": article_domain,
+                "text": text,
+            })
+
+    # Clear the scroll to release resources on the server
+    try:
+        es_client.clear_scroll(scroll_id=scroll_id)
+    except Exception as e:
+        logger.error(f"Error during clearing scroll: {e}")
+
+    return documents
+
+
+async def process_batch(documents):
+    results = []
+    for doc in documents:
+        minhash = await minhash_signature(doc.get('text'), doc.get('language'))
+        results.append({
+            "article_id": doc.get('article_id'),
+            "article_domain": doc.get('article_domain'),
+            "minhash": minhash
+        })
+    logger.info(f"Processed {len(results)} documents.")
+    return results
+
+
+async def process_batches(lsh_with_ttl, documents, workers=4, batch_size=1000):
+    async with Pool(workers) as pool:
+        tasks = []
+        for i in range(0, len(documents), batch_size):
+            logger.info(f"Processing batch {i // batch_size + 1}...")
+            batch = documents[i:i + batch_size]
+            tasks.append(pool.apply(process_batch, (batch,)))
+
+        results = await asyncio.gather(*tasks)
+        logger.info("Finished processing all batches.")
+        # Flatten the list of results
+        results = [item for sublist in results for item in sublist]
+        logger.info(f"Inserting {len(results)} MinHash signatures into LSH...")
+        # Insert into LSH with batch insertion
+        with lsh_with_ttl.lsh.insertion_session() as session:
+            for doc in results:
+                key = f"{doc.get('article_id')}|{doc.get('article_domain')}"
+                session.insert(key, doc.get('minhash'))
+
+
+@timeit_decorator
+async def fast_recovery():
+    """
+    Initialize LSH with TTL and load documents from Elasticsearch.
+    """
+    lsh_with_ttl = MinHashLSHTTL(threshold=0.9, num_perm=128)
+    documents = await get_texts_from_es()
+    await process_batches(lsh_with_ttl, documents)
+    return lsh_with_ttl
+
+
 # Retrieve LSH object from Redis
 def get_lsh_from_redis(lsh_key=None):
     lsh_with_ttl = None
@@ -98,7 +262,7 @@ def get_lsh_from_redis(lsh_key=None):
     except TypeError:
         metrics.count(Consts.TOTAL_LSH_OBJECT_CREATED)
         logger.error("LSH object not found in Redis. Creating new LSH.")
-        lsh_with_ttl = MinHashLSHTTL(threshold=0.95, num_perm=128)
+        lsh_with_ttl = asyncio.get_event_loop().run_until_complete(fast_recovery())()
     except Exception as e:
         logger.error(f"Error while getting LSH from Redis: {str(e)}")
     finally:
@@ -161,3 +325,9 @@ def store_article_in_redis(url, queue_name="similarity"):
             redis_connection.sadd(queue_name, url)
     except Exception as e:
         logger.critical(f"Failed to store article in Redis: {str(e)}")
+
+
+# if __name__ == "__main__":
+#     # Start the event loop
+#     loop = asyncio.get_event_loop()
+#     lsh = loop.run_until_complete(fast_recovery())
